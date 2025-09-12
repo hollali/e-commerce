77eb8add7d8bf89a86989185a60f2740bec8a2b6
hollali <dheztinykartel@gmail.com>
2025-09-12 14:25:49 +0000

    `Update interface definitions and ImageGallery component to match updated fullProduct interface`

--- src/app/interface.ts
+++ src/app/interface.ts
@@ -1,21 +1,21 @@
 export interface simplifiedProduct {
-    _id: string;
-    imageUrl: string;
-    price: number;
-    slug: string;
-    categoryName:string;
-    name: string;
+  _id: string;
+  imageUrl: string;
+  price: number;
+  slug: string;
+  categoryName: string;
+  name: string;
 }
 
 export interface fullProduct {
-    _id: string;
-    images: any;
-    price: number;
-    slug: string;
-    categoryName:string;
-    name: string;
-    description: string;
-    price_id:string;
+  _id: string;
+  images: any;
+  price: number;
+  slug: string;
+  categoryName: string;
+  name: string;
+  description: string;
+  price_id: string;
 }
 
 interface WishlistItem {
