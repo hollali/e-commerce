6cc7be7a3e29ddefa2677f96e32ad197704913d9
hollali <dheztinykartel@gmail.com>
2025-09-13 11:40:05 +0000

    `Update link in empty cart message to root URL`

--- src/components/shoppingCartModal.tsx
+++ src/components/shoppingCartModal.tsx
@@ -109,7 +109,7 @@ export default function ShoppingCartModal() {
                   <h1 className="text-2xl font-semibold">
                     Your cart is empty !!
                   </h1>
-                  <Link href="/shop">
+                  <Link href="/">
                     <button className="mt-6 bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
                       Shop Now
                     </button>
