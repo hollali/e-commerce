0368a51ea3d49e29edcc2fed5bae60efa2a04c91
hollali <dheztinykartel@gmail.com>
2025-09-16 03:09:22 +0000

    `Fixed import path inconsistencies and added optional icon to AddToBag component`

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,13 +1,13 @@
-import { client } from '@/lib/sanity';
-import CheckoutNow from '@/components/CheckoutNow';
-import { fullProduct } from '@/app/interface';
-import ImageGallery from '@/components/imageGallery';
+import { client } from "@/lib/sanity";
+import CheckoutNow from "@/components/CheckoutNow";
+import { fullProduct } from "@/app/interface";
+import ImageGallery from "@/components/imageGallery";
 import { Button } from "@/components/ui/button";
-import { Star, Truck } from 'lucide-react';
-import AddToBag from '@/components/AddToBag';
+import { Star, Truck } from "lucide-react";
+import AddToBag from "@/components/AddToBag";
 
 async function getData(slug: string) {
-    const query = `*[_type == "product" && slug.current == $slug][0]{
+  const query = `*[_type == "product" && slug.current == $slug][0]{
         _id,
         images,
         price,
@@ -18,78 +18,84 @@ async function getData(slug: string) {
         price_id
     }`;
 
-    const data = await client.fetch(query, { slug });
+  const data = await client.fetch(query, { slug });
 
-    return data;
+  return data;
 }
 
 export const dynamic = "force-dynamic";
 
 export default async function ProductPage({
-    params,
+  params,
 }: {
-    params: { slug: string };
+  params: { slug: string };
 }) {
-    const data: fullProduct = await getData(params.slug);
-    const cedisSign = '\u20B5';
-    return (
-        <div className="bg-white">
-            <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
-                <div className='grid gap-8 md:grid-cols-2'>
-                    <ImageGallery images={data.images} />
-                    <div className='md:py-8'>
-                        <div className="mb-2 md:mb-3">
-                            <span className='mb-0.5 inline-block text-gray-500'>{data.categoryName}</span>
-                            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
-                                {data.name}
-                            </h2>
-                        </div>
-                        <div className='mb-6 flex items-center gap-3 md:mb-10'>
-                            <Button className='rounded-full bg-blue-600 gap-x-2'>
-                                <span className='text-sm'>4.5</span>
-                                <Star className='h-5 w-5' />
-                            </Button>
-                            <span className="text-sm text-gray-500">56 Ratings</span>
-                        </div>
-                        <div className='mb-4'>
-                            <div className='flex items-end gap-2'>
-                                <span className="text-xl font-bold text-gray-800 md:text-2xl">{cedisSign} {data.price}</span>
-                                <span className="mb-0.5 text-red-500 line-through">{cedisSign} {data.price + 20}</span>
-                            </div>
-                            <span className='text-sm text-gray-500'>
-                                Incl. VAT. Shipping is different
-                            </span>
-                        </div>
-                        <div className="mb-6 flex items-center gap-2 text-gray-500">
-                            <Truck className='h-6 w-6' />
-                            <span className="text-sm">4-6 Days delivery</span>
-                        </div>
-                        <div className='flex gap-2.5'>
-                            <AddToBag
-                                currency="GHS"
-                                description={data.description}
-                                image={data.images[0]}
-                                name={data.name}
-                                price={data.price}
-                                key={data._id}
-                                price_id={data.price_id}
-                            />
-                            <CheckoutNow
-                                currency="GHS"
-                                description={data.description}
-                                image={data.images[0]}
-                                name={data.name}
-                                price={data.price}
-                                key={data._id}
-                                price_id={data.price_id}
-                            />
-                        </div>
-                        <p className='mt-12 text-gray-500 tracking-wide'>
-                            {data.description}
-                        </p>
-                    </div>
-                </div>
+  const data: fullProduct = await getData(params.slug);
+  const cedisSign = "\u20B5";
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+        <div className="grid gap-8 md:grid-cols-2">
+          <ImageGallery images={data.images} />
+          <div className="md:py-8">
+            <div className="mb-2 md:mb-3">
+              <span className="mb-0.5 inline-block text-gray-500">
+                {data.categoryName}
+              </span>
+              <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
+                {data.name}
+              </h2>
             </div>
+            <div className="mb-6 flex items-center gap-3 md:mb-10">
+              <Button className="rounded-full bg-blue-600 gap-x-2">
+                <span className="text-sm">4.5</span>
+                <Star className="h-5 w-5" />
+              </Button>
+              <span className="text-sm text-gray-500">56 Ratings</span>
+            </div>
+            <div className="mb-4">
+              <div className="flex items-end gap-2">
+                <span className="text-xl font-bold text-gray-800 md:text-2xl">
+                  {cedisSign} {data.price}
+                </span>
+                <span className="mb-0.5 text-red-500 line-through">
+                  {cedisSign} {data.price + 20}
+                </span>
+              </div>
+              <span className="text-sm text-gray-500">
+                Incl. VAT. Shipping is different
+              </span>
+            </div>
+            <div className="mb-6 flex items-center gap-2 text-gray-500">
+              <Truck className="h-6 w-6" />
+              <span className="text-sm">4-6 Days delivery</span>
+            </div>
+            <div className="flex gap-2.5">
+              <AddToBag
+                currency="GHS"
+                description={data.description}
+                image={data.images[0]}
+                name={data.name}
+                price={data.price}
+                key={data._id}
+                price_id={data.price_id}
+              />
+              <CheckoutNow
+                currency="GHS"
+                description={data.description}
+                image={data.images[0]}
+                name={data.name}
+                price={data.price}
+                key={data._id}
+                price_id={data.price_id}
+              />
+            </div>
+            <p className="mt-12 text-gray-500 tracking-wide">
+              {data.description}
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/AddToBag.tsx
+++ src/components/AddToBag.tsx
@@ -1,46 +1,50 @@
 "use client";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import { useShoppingCart } from "use-shopping-cart";
-import { urlFor } from '../lib/sanity';
+import { urlFor } from "../lib/sanity";
+import { ReactNode } from "react";
 
 export interface ProductCart {
-    name: string;
-    description: string;
-    price: number;
-    currency: string;
-    image: any;
-    price_id: string;
+  name: string;
+  description: string;
+  price: number;
+  currency: string;
+  image: any;
+  price_id: string;
+  icon?: ReactNode; // 👈 allow optional icon
 }
 
 export default function AddToBag({
-    currency,
-    description,
-    image,
-    name,
-    price,
-    price_id,
+  currency,
+  description,
+  image,
+  name,
+  price,
+  price_id,
+  icon,
 }: ProductCart) {
-    const { addItem, handleCartClick } = useShoppingCart();
+  const { addItem, handleCartClick } = useShoppingCart();
 
-    const product = {
-        name,
-        description,
-        price,
-        currency,
-        image: urlFor(image).url(),
-        price_id,
-    };
+  const product = {
+    name,
+    description,
+    price,
+    currency,
+    image: urlFor(image).url(),
+    price_id,
+  };
 
-    return (
-        <Button
-            className=" bg-blue-800 hover:bg-blue-600"
-            onClick={() => {
-                addItem(product);
-                handleCartClick();
-            }}
-        >
-            Add To Cart
-        </Button>
-    );
+  return (
+    <Button
+      className="flex items-center gap-2 bg-blue-800 hover:bg-blue-600"
+      onClick={() => {
+        addItem(product);
+        handleCartClick();
+      }}
+    >
+      {icon && icon} {/* 👈 render icon if provided */}
+      Add To Cart
+    </Button>
+  );
 }
