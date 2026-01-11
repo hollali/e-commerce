d4d242f775c62f1afe12ea48396cfad8f68cc6ea
hollali <dheztinykartel@gmail.com>
2026-01-11 03:29:06 +0000

    `Remove dark mode border color from navbar header`

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 lg:static border-b bg-background mb-8 dark:border-gray-700">
+      <header className="fixed top-0 left-0 right-0 z-50 lg:static border-b bg-background mb-8">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16 lg:h-24">
             {/* Left: Menu + Logo */}
