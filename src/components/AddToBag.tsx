1e5ee28b5142fa6e9c5045239df51c25d726c11d
hollali <dheztinykartel@gmail.com>
2026-01-16 04:55:12 +0000

    `Update AddToBag component button styles`

--- src/components/AddToBag.tsx
+++ src/components/AddToBag.tsx
@@ -37,7 +37,7 @@ export default function AddToBag({
 
   return (
     <Button
-      className="flex items-center gap-2 bg-blue-800 hover:bg-blue-600 dark:bg-gradient-to-br dark:from-purple-600 dark:via-pink-600 dark:to-blue-600 dark:hover:from-purple-500 dark:hover:via-pink-500 dark:hover:to-blue-500 text-white"
+      className="flex items-center gap-2 bg-gradient-to-br from-pink-700 via-purple-600 to-blue-800 hover:from-pink-600 hover:via-blue-500 hover:to-pink-700 dark:bg-gradient-to-br dark:from-purple-600 dark:via-pink-600 dark:to-blue-600 dark:hover:from-purple-500 dark:hover:via-pink-500 dark:hover:to-blue-500 text-white"
       onClick={() => {
         addItem(product);
         handleCartClick();
